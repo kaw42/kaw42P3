5afe18f34253141520239b87a55b7fc83b6f29b3
kaw42 <kaw42@njit.edu>
2022-04-19 13:43:34 -0400

    changed workflows and set up secrets

--- app/__init__.py
+++ app/__init__.py
@@ -90,27 +90,8 @@ def create_app():
         elif request.path.startswith('/bootstrap'):
             return response
 
-        now = time.time()
-        duration = round(now - g.start, 2)
-        dt = datetime.datetime.fromtimestamp(now)
-        timestamp = rfc3339(dt, utc=True)
-
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-        host = request.host.split(':', 1)[0]
-        args = dict(request.args)
-
-        request_id = request.headers.get('X-Request-ID')
-        if request_id:
-            log_params.append(('request_id', request_id))
-
-        parts = []
-        for name, value in log_params:
-            part = name + ': ' + str(value) + ', '
-            parts.append(part)
-        line = " ".join(parts)
-        #this triggers a log entry to be created with whatever is in the line variable
-        app.logger.info('this is the plain message')
-
+        log = logging.getLogger("myApp")
+        log.info("My App Logger")
         return response
 
     return app
