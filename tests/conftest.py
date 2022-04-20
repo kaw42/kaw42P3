be4913489912c03520142d1af804e29a0014ee84
kaw42 <kaw42@njit.edu>
2022-04-20 00:27:59 -0400

    added config file

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,31 @@
 
 import pytest
 from app import create_app
-
+from app.db import db
 
 @pytest.fixture()
 def application():
     """This makes the app"""
     application = create_app()
-    application.config.update({
-        "TESTING": True,
-    })
-    yield application
+    application.config.update(
+
+        ENV='development',
+
+    )
+    with application.app_context():
+        db.create_all()
+        yield application
+        db.session.remove()
+
+        db.drop_all()
+
+@pytest.fixture()
+def add_user(application):
+    with application.app_context():
+        #new record
+        user = User('keith@webizly.com', 'testtest')
+        db.session.add(user)
+        db.session.commit()
 
 
 @pytest.fixture()
